c1126e8d8808f2a514e5dbe4d90b119c3b6ef3d3
Daniel Palacio <45110344+palaciodaniel@users.noreply.github.com>
2020-08-08 06:13:47 -0300

    Uploading items from the multiple choice

---
+++ packages/channard_questions.py
@@ -0,0 +1,137 @@
+
+channard_items = [
+
+                    """PERFORMANCE \n
+0. I feel as productive as always.
+1. I feel less productive, but I think it is temporary.
+2. Definitely I am behind schedule.
+3. I have not managed to finish anything at all.""",
+
+                    """BOTTLED WATER \n
+0. Nothing remarkable to say about it.
+1. It is a scam. It is just tap water.
+2. Chemicals are being put on it to affect our brains.
+3. It smells and tastes nauseating, like rotten flesh.""",
+
+                    """SLEEP \n
+0. I am sleeping without interruptions.
+1. Poor sleeping quality. Distressing dreams.
+2. Nightmares and instances of sleep terror.
+3. I wake up, I cannot move and I see an evil entity.""",
+
+                    """HAIR \n
+0. I do not have current observations about it.
+1. I cannot stop obsessively combing it all the time.
+2. Despite my hygiene, people are disgusted by it.
+3. I have a strong urge to rip it from my head.""",
+
+                    """SKIN \n
+0. My skin feels as usual.
+1. My skin feels dry and worn out.
+2. I see dark spots and open cracks in my skin.
+3. There are worms crawling under my skin.""",
+
+                    """VOICES \n
+0. The only voices in my head are simply my thoughts.
+1. I am pondering about a lot of negative things.
+2. I hear voices in my head, like a distant echo.
+3. I hear voices in my head insulting me.""",
+
+                    """MIRROR \n
+0. My reflection is the same as always.
+1. I look thinner/fatter than usual.
+2. My reflection is considerably deformed. 
+3. My reflection is a corpse.""",
+
+                    """BUS DRIVERS \n
+0. They are just regular people doing their job.
+1. They are reckless, because they crave adrenaline.
+2. They intentionally take me to wrong locations.
+3. They plan to eventually kill me by crashing the bus.""",
+
+                    """RELATIVES \n
+0. My family is behaving as always.
+1. My family is hiding something bad from me.
+2. My family is planning something bad against me.
+3. They are impostors, who pretend to be my relatives.""",
+
+                    """BIRDS \n
+0. They are inoffensive animals.
+1. They are disgusting and spread diseases.
+2. They are secret weapons of mass surveillance.
+3. If they can, they will devour my eyes while I sleep.""",
+
+                    """DISASTER \n
+0. There is not evidence of any world-wide threat.
+1. There is a potential threat, but nothing is confirmed.
+2. Something terrible will happen, but it is avoidable.
+3. An unavoidable disaster will happen very soon.""",
+
+                    """NEWS BROADCAST \n
+0. They are operating as always.
+1. They spread gossip about me when I am not watching them.
+2. I see them flagrantly spreading misinformation about me.
+3. They start talking aggressively to me when I watch them.""",
+
+                    """COUNTRY \n
+0. Argentina is a country located in South America.
+1. Argentina is a potentially hostile nation.
+2. Their people secretly collaborate with evil entities.
+3. Argentina does not exist at all. All maps are lying.""",
+
+                    """BOOKS \n
+0. They are just part of our normal lives.
+1. They are written to serve as controlled opposition.
+2. They have hidden dark messages written between lines.
+3. They instruct me about people I have to assassinate.""",
+
+                    """CLOCKS \n
+0. One of the tools that allow me to be productive.
+1. Their failed design does not match the real passing of time.
+2. Some agent secretly controls them from a remote place.
+3. They can be used to manipulate historic events.""",
+
+                    """KNIFES \n
+0. Another everyday tool that is very useful.
+1a. They evoke interesting sounds of people screaming.
+1b. They evoke horrific images of blood and wounds.
+2a. I usually fantasize to hurt people with them.
+2b. I usually fantasize to use them to mark my body.
+3a. I used them to physically attack evil entities.
+3b. I cut myself with them to alleviate my suffering.""",
+
+                    """KALEIDOSCOPE \n
+0. An interesting but otherwise absolutely normal object.
+1. I am convinced some of the reflexes inside contradict reality.
+2. They allow us to communicate with other dimensions.
+3. They allow us to physically enter other dimensions.""",
+
+                    """GOD \n
+0. My opinion about God is still the same.
+1a. I recently witnessed a God's manifestation.
+1b. God has abandoned us and let us to rot.
+2a. God explicitly told me I have a special mission here.
+2b. God explicitly told me He enjoys mankind's suffering.
+3a. If I commit suicide now, I will finally meet Him.
+3b. God has died.""",
+
+                    """AFTERLIFE \n
+0. It is a complete mistery, nothing is certain.
+1. It may be both good and bad, like our existence here.
+2. We will find some peace after so much suffering.
+3. We are doomed to an eternal torment after death.""",
+
+                    """SKY \n
+0. The sky looks as usual.
+1. I see mysterious dots in the sky.
+2. The sky is showing strange colors.
+3. The clouds are bleeding.""",
+
+                    """SCHRECKEN \n
+0. It does not exist.
+1. It might exist, but it is unlikely.
+2. It exists, but It cannot reach us yet.
+3. It is coming. It is almost here."""
+
+]
+
